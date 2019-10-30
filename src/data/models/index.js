05d48091daf94acaa98e92c2ee34673729161a51
Valek2412 <valoleg99@gmail.com>
2019-10-30 20:12:11 +0200

    Added dependencies

--- src/data/models/index.js
+++ src/data/models/index.js
@@ -1,9 +1,66 @@
 import sequelize from '../sequelize';
-// import User from './User';
+import User from './User';
+import Bet from './Bet';
+import Lot from './Lot';
+import Notification from './Notification';
+import NotificationToUser from './NotificationToUser';
+import Room from './Room';
+import RoomToUser from './RoomToUser';
+import UserSession from './UserSession';
+
+User.hasMany(Bet, {
+  foreignKey: 'userId',
+  as: 'bets',
+  onUpdate: 'cascade',
+  onDelete: 'cascade',
+});
+
+User.hasOne(UserSession, {
+  foreignKey: 'userId',
+  as: 'session',
+  onUpdate: 'cascade',
+  onDelete: 'cascade',
+});
+
+User.hasMany(Lot, {
+  foreignKey: 'userId',
+  as: 'lots',
+  onUpdate: 'cascade',
+  onDelete: 'cascade',
+});
+
+Lot.hasMany(Bet, {
+  foreignKey: 'id',
+  as: 'betPerLot',
+  onUpdate: 'cascade',
+  onDelete: 'cascade',
+});
+
+Room.hasOne(Lot, {
+  foreignKey: 'id',
+  as: 'lotInRoom',
+  onUpdate: 'cascade',
+  onDelete: 'cascade',
+});
+
+User.belongsToMany(Notification, { through: NotificationToUser });
+Notification.belongsToMany(User, { through: NotificationToUser });
+
+User.belongsToMany(Room, { through: RoomToUser });
+Room.belongsToMany(User, { through: RoomToUser });
 
 function sync(...args) {
   return sequelize.sync(...args);
 }
 
 export default { sync };
-// export { User };
+export {
+  User,
+  RoomToUser,
+  Notification,
+  NotificationToUser,
+  UserSession,
+  Bet,
+  Lot,
+  Room,
+};
