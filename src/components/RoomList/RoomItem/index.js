2a2a30269d838ce2ae08e514998b873727906f26
Valek2412 <valoleg99@gmail.com>
2019-11-12 22:19:50 +0200

    import path correction

--- src/components/RoomList/RoomItem/index.js
+++ src/components/RoomList/RoomItem/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Link from '../../Link';
+import Link from 'components/Link';
 
 function RoomItem({ room }) {
   const handleClick = async () => {};
