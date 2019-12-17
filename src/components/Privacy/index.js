340312f7309d26e9bd239f28af6cdb8135b8a83d
Valek2412 <valoleg99@gmail.com>
2019-12-17 18:47:10 +0200

    fix privacy view

---
+++ src/components/Privacy/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Privacy() {
+  return (
+    <div
+      className="jumbotron jumbotron-fluid h-100"
+      style={{ marginBottom: -32 }}
+    >
+      <div className="container mb-5">
+        <h1 className="display-4 mt-2">Privacy</h1>
+        <hr className="my-5" />
+        <p className="lead">
+          When you use our services, you’re trusting us with your information.
+          We understand this is a big responsibility and work hard to protect
+          your information and put you in control.
+        </p>
+        <hr className="my-5" />
+        <p className="mb-5">
+          This Privacy Policy is meant to help you understand what information
+          we collect, why we collect it, and how you can update, manage, export,
+          and delete your information.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Privacy;
--- src/routes/privacy/index.js
+++ src/routes/privacy/index.js
@@ -1,16 +1,7 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 import React from 'react';
 import Layout from '../../components/Layout';
-import Page from '../../components/Page';
 import privacy from './privacy.md';
+import Privacy from '../../components/Privacy';
 
 function action() {
   return {
@@ -18,8 +9,7 @@ function action() {
     title: privacy.title,
     component: (
       <Layout>
-        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-        <Page {...privacy} />
+        <Privacy />
       </Layout>
     ),
   };
